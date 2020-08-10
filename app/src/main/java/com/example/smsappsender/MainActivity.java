577e9a77bbc044937ec954e52ed58193cdfbbcbd
ayoub <ayoobchoura@gmail.com>
2020-08-10 11:49:59 -0400

    my second commit

--- app/src/main/java/com/example/smsappsender/MainActivity.java
+++ app/src/main/java/com/example/smsappsender/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.smsappsender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
     //Pptés qui vont faire le lien
@@ -43,9 +49,26 @@ public class MainActivity extends AppCompatActivity {
     private void createOnClickEnvoiButton(){
         envoi.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v) {
-                SmsManager.getDefault(
+                //controler si la permission est OK
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS)== PackageManager.PERMISSION_GRANTED) {
+                    SmsManager.getDefault(
 
-                ).sendTextMessage(phone.getText().toString(),null,message.getText().toString(),null,null);
+                    ).sendTextMessage(phone.getText().toString(), null, message.getText().toString(), null, null);
+                //msg pour informer l'utilisateur que l sms est envoyé avec succes
+                    Toast.makeText(MainActivity.this,"SMS envoyé avec succes",Toast.LENGTH_SHORT).show();
+                 //vider le txt
+                 message.setText("");
+                }else{
+                    //demande de donner la permission
+                    if(!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.SEND_SMS)){
+                        String[] permissions={Manifest.permission.SEND_SMS};
+                        //afficher la demande de permission
+                       ActivityCompat.requestPermissions(MainActivity.this,permissions,2);
+
+                    }else{
+                        //afficher que la permission est obligatoire
+                    }
+                }
             }
         });
 
